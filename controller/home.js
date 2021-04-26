f9b1c7b6aeeb3199e40d61e7a2fcedcf2fc2eb03
Rupesh Kumar Dash <rupeshkumardash123@gmail.com>
2021-04-26 20:42:05 +0530

    Add data to the database

--- controller/home.js
+++ controller/home.js
@@ -1,21 +1,41 @@
+const Task = require("../models/task");
 
-const taskList = [];
+
 module.exports.task = function(req,res){
-    console.log(req.body);
+    // console.log(req.body);
     
-    taskList.push(req.body);
-    console.log(taskList);
-    return res.redirect('back');
+    // taskList.push(req.body);
+    // console.log(taskList);
+    // return res.redirect('back');
+
+    Task.create({
+        task : req.body.task,
+        preference : req.body.preference,
+        date : req.body.date,
+    },function(err,newTask){
+        if(err){console.log('Error creating database!!'); return;}
+        console.log("****************",newTask);
+        return res.redirect('back');
+    });
    
 }
 
 module.exports.home = function(req,res){
    
-    return res.render('home',{
-        title:'home',
-        task_list : taskList
-        
+    Task.find({},function(err,task){
+        if(err){
+            console.log("Error in fetching tasks");
+            return;
+        }
+
+        return res.render('home',{
+            title:'home',
+            task_list : task
+            
+        });
     });
+
+    
 }
 
 // controller to the post request recieved from the task submission form
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port  = 8000;
 const db = require('./config/mongoose');
+const Task = require('./models/task');
 app.use(express.urlencoded());
 //middleware to control the routes
 app.use('/',require('./routes/index'));
